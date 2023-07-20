c5ff578a85f1d937e4f0a5b87fad825877c605d4
Dvd848 <Dvd848@users.noreply.github.com>
2023-07-20 22:03:49 +0300

    Initial Commit

---
+++ bonus-cheat/Bonus.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bonus_cheat
+{
+    internal class Bonus
+    {
+        const string DOSBOX_PROCESS_NAME = "DOSBox";
+        const uint DOSBOX_MEMSIZE = 0x1001000;
+
+        const int BONUS_MEM_OFFSET_PLAYER1_LETTERS = 0x4C404;
+        const int BONUS_MEM_OFFSET_PLAYER2_LETTERS = 0x4C40D;
+        const int BONUS_MEM_OFFSET_RACK = 0x4C434;
+        const int BONUS_MEM_OFFSET_BOARD = 0x4C444;
+
+        const int BONUS_NUM_PLAYER_LETTERS = 8;
+        const int BONUS_BOARD_SIZE = 0x9b;
+
+        const int BONUS_BOARD_DIMENTION = 12;
+
+        const int CODE_PAGE_862_START = 0x80;
+        const int CODE_PAGE_862_END = 0x9A;
+
+        private ProcessMemoryWriter memoryWriter;
+        private IntPtr baseAddress;
+
+        private static readonly char[] CodePage862ToUTF8Mapping = new char[]
+        {
+            // Custom mapping from Code Page 862 to UTF-8
+            'א', 'ב', 'ג', 'ד', 'ה', 'ו', 'ז', 'ח', 'ט', 'י', 'ך', 'כ', 'ל', 'ם', 'מ', 'ן',
+            'נ', 'ס', 'ע', 'ף', 'פ', 'ץ', 'צ', 'ק', 'ר', 'ש', 'ת'
+        };
+
+        public enum Player
+        {
+            Player1,
+            Player2
+        }
+
+        public Bonus()
+        {
+            this.memoryWriter = new ProcessMemoryWriter(DOSBOX_PROCESS_NAME);
+            List<IntPtr> baseAddresses = this.memoryWriter.LocateWritableMemoryRangeBySize(DOSBOX_MEMSIZE);
+            if (baseAddresses.Count != 1)
+            {
+                throw new Exception("Can't identify memory range for DOSBox memory");
+            }
+
+            this.baseAddress = baseAddresses[0];
+
+            /*
+            byte[] p1letters = this.memoryWriter.ReadMemory(baseAddress + BONUS_MEM_OFFSET_PLAYER1_LETTERS, BONUS_NUM_PLAYER_LETTERS);
+            byte[] p2letters = this.memoryWriter.ReadMemory(baseAddress + BONUS_MEM_OFFSET_PLAYER2_LETTERS, BONUS_NUM_PLAYER_LETTERS);
+            byte[] rack = this.memoryWriter.ReadMemory(baseAddress + BONUS_MEM_OFFSET_RACK, BONUS_NUM_PLAYER_LETTERS);
+
+            byte[] board = this.memoryWriter.ReadMemory(baseAddress + BONUS_MEM_OFFSET_BOARD, BONUS_BOARD_SIZE);
+
+            char[] letters = getPlayerLetters(Player.Player1);
+            char[,] boardArr = getBoard();
+            */
+
+            //this.memoryWriter.WriteMemory(baseAddress + BONUS_MEM_OFFSET_PLAYER1_LETTERS, new byte[]{ 0x90, 0x91});
+
+        }
+
+        public char[] getPlayerLetters(Player player)
+        {
+            char[] res = new char[BONUS_NUM_PLAYER_LETTERS];
+            int offset = getPlayerMemOffset(player);
+
+            byte[] letters = this.memoryWriter.ReadMemory(baseAddress + offset, BONUS_NUM_PLAYER_LETTERS);
+            
+            for (var i = 0; i < res.Length; i++)
+            {
+                res[i] = ConvertCodePage862ToUTF8(letters[i]);
+            }
+
+            return res;
+        }
+
+
+        public void setPlayerLetter(Player player, int index, char letter)
+        {
+            if (index < 0 || index > BONUS_NUM_PLAYER_LETTERS)
+            {
+                throw new ArgumentException("Invalid index for setting player letter");
+            }
+
+            byte b = ConvertUTF8ToCodePage862(letter);
+
+            this.memoryWriter.WriteMemory(baseAddress + getPlayerMemOffset(player) + index, new byte[] { b });
+        }
+        public char[] getRackLetters()
+        {
+            char[] res = new char[BONUS_NUM_PLAYER_LETTERS];
+
+            byte[] letters = this.memoryWriter.ReadMemory(baseAddress + BONUS_MEM_OFFSET_RACK, BONUS_NUM_PLAYER_LETTERS);
+
+            for (var i = 0; i < res.Length; i++)
+            {
+                res[i] = ConvertCodePage862ToUTF8(letters[i]);
+            }
+
+            return res;
+        }
+
+        public char[,] getBoard()
+        {
+            char[,] res = new char[BONUS_BOARD_DIMENTION, BONUS_BOARD_DIMENTION];
+
+            byte[] letters = this.memoryWriter.ReadMemory(baseAddress + BONUS_MEM_OFFSET_BOARD, BONUS_BOARD_SIZE);
+
+            int index = 0;
+            for (var r = 0; r < BONUS_BOARD_DIMENTION; r++)
+            {
+                for (var c = 0; c < BONUS_BOARD_DIMENTION; c++)
+                {
+                    res[r,c] = ConvertCodePage862ToUTF8(letters[index]);
+                    index++;
+                }
+                index++;
+            }
+
+            return res;
+        }
+
+        public void setBoardLetter(int row, int col, char letter)
+        {
+            if ( (row < 0 || row > BONUS_BOARD_DIMENTION) || (col < 0 || col > BONUS_BOARD_DIMENTION) )
+            {
+                throw new ArgumentException("Invalid index for setting board letter");
+            }
+
+            byte b = ConvertUTF8ToCodePage862(letter);
+            int index = row * BONUS_BOARD_DIMENTION + col;
+            index = index + (index / BONUS_BOARD_DIMENTION);
+
+            Debug.Assert(this.memoryWriter.ReadMemory(baseAddress + BONUS_MEM_OFFSET_BOARD + index, 1)[0] != 0x0);
+
+            this.memoryWriter.WriteMemory(baseAddress + BONUS_MEM_OFFSET_BOARD + index, new byte[] { b });
+        }
+
+        private int getPlayerMemOffset(Player player)
+        {
+            return new Dictionary<Player, int>
+            {
+                { Player.Player1, BONUS_MEM_OFFSET_PLAYER1_LETTERS },
+                { Player.Player2, BONUS_MEM_OFFSET_PLAYER2_LETTERS }
+            }[player];
+        }
+
+        private static char ConvertCodePage862ToUTF8(byte codePage862Byte)
+        {
+            if (codePage862Byte == 0x20)
+            {
+                return ' ';
+            }
+            else if (codePage862Byte == 0x30)
+            {
+                return '#';
+            }
+
+            if (codePage862Byte < CODE_PAGE_862_START || codePage862Byte > CODE_PAGE_862_END)
+            {
+                throw new ArgumentException("Invalid Code Page 862 byte");
+            }
+
+            return CodePage862ToUTF8Mapping[codePage862Byte - CODE_PAGE_862_START];
+        }
+
+        private static byte ConvertUTF8ToCodePage862(char utf8Char)
+        {
+            if (utf8Char == ' ')
+            {
+                return 0x20;
+            }
+
+            int index = Array.IndexOf(CodePage862ToUTF8Mapping, utf8Char);
+            if (index == -1)
+            {
+                throw new ArgumentException("Invalid UTF-8 character");
+            }
+
+            // Get the corresponding Code Page 862 byte
+            return (byte)(index + CODE_PAGE_862_START);
+        }
+    }
+}
---
+++ bonus-cheat/ProcessUtils.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bonus_cheat
+{
+
+    public enum AllocationProtectEnum : uint
+    {
+        PAGE_EXECUTE = 0x00000010,
+        PAGE_EXECUTE_READ = 0x00000020,
+        PAGE_EXECUTE_READWRITE = 0x00000040,
+        PAGE_EXECUTE_WRITECOPY = 0x00000080,
+        PAGE_NOACCESS = 0x00000001,
+        PAGE_READONLY = 0x00000002,
+        PAGE_READWRITE = 0x00000004,
+        PAGE_WRITECOPY = 0x00000008,
+        PAGE_GUARD = 0x00000100,
+        PAGE_NOCACHE = 0x00000200,
+        PAGE_WRITECOMBINE = 0x00000400
+    }
+
+    public enum StateEnum : uint
+    {
+        MEM_COMMIT = 0x1000,
+        MEM_FREE = 0x10000,
+        MEM_RESERVE = 0x2000
+    }
+
+    public enum TypeEnum : uint
+    {
+        MEM_IMAGE = 0x1000000,
+        MEM_MAPPED = 0x40000,
+        MEM_PRIVATE = 0x20000
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MEMORY_BASIC_INFORMATION
+    {
+        public IntPtr BaseAddress;
+        public IntPtr AllocationBase;
+        public AllocationProtectEnum AllocationProtect;
+        public IntPtr RegionSize;
+        public StateEnum State;
+        public AllocationProtectEnum Protect;
+        public TypeEnum Type;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MEMORY_BASIC_INFORMATION64
+    {
+        public ulong BaseAddress;
+        public ulong AllocationBase;
+        public int AllocationProtect;
+        public int __alignment1;
+        public ulong RegionSize;
+        public int State;
+        public int Protect;
+        public int Type;
+        public int __alignment2;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SYSTEM_INFO
+    {
+        public ushort processorArchitecture;
+        ushort reserved;
+        public uint pageSize;
+        public IntPtr minimumApplicationAddress;
+        public IntPtr maximumApplicationAddress;
+        public IntPtr activeProcessorMask;
+        public uint numberOfProcessors;
+        public uint processorType;
+        public uint allocationGranularity;
+        public ushort processorLevel;
+        public ushort processorRevision;
+    }
+
+
+    internal class ProcessMemoryWriter
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern int VirtualQueryEx(IntPtr hProcess, IntPtr lpAddress, out MEMORY_BASIC_INFORMATION lpBuffer, uint dwLength);
+
+        [DllImport("kernel32.dll")]
+        static extern void GetSystemInfo(out SYSTEM_INFO lpSystemInfo);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory
+        (IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint dwSize, ref uint lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool WriteProcessMemory
+        (IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint dwSize, ref uint lpNumberOfBytesWritten);
+
+        IntPtr processHandle = IntPtr.Zero;
+        public ProcessMemoryWriter(string processName)
+        {
+            this.processHandle = GetProcessHandleByName(processName);
+        }
+
+        public List<IntPtr> LocateWritableMemoryRangeBySize(uint size)
+        {
+            List <IntPtr> res = new List<IntPtr> ();
+
+            SYSTEM_INFO sysInfo;
+            GetSystemInfo(out sysInfo);
+
+            IntPtr MaxAddress = sysInfo.maximumApplicationAddress;
+            IntPtr currentAddress = sysInfo.minimumApplicationAddress;
+
+            do
+            {
+                MEMORY_BASIC_INFORMATION memInfo;
+                int result = VirtualQueryEx(this.processHandle, currentAddress, out memInfo, (uint)Marshal.SizeOf(typeof(MEMORY_BASIC_INFORMATION)));
+                if (result == 0)
+                {
+                    break;
+                }
+
+                if (memInfo.RegionSize.ToInt64() == size && memInfo.AllocationProtect == AllocationProtectEnum.PAGE_READWRITE)
+                {
+                    res.Add (memInfo.BaseAddress);
+                    Debug.WriteLine(String.Format("Found base address 0x{0:X} of size 0x{1:X} for process handle 0x{2:X}", memInfo.BaseAddress,
+                        size, this.processHandle));
+                }
+
+                currentAddress = new IntPtr(memInfo.BaseAddress.ToInt64() + memInfo.RegionSize.ToInt64()); ;
+            } while (currentAddress.CompareTo(MaxAddress) < 0);
+
+            return res;
+        }
+
+        public byte[] ReadMemory(IntPtr address, uint size)
+        {
+            uint bytesRead = 0;
+            byte[] buffer = new byte[size];
+
+            if ( (ReadProcessMemory(this.processHandle, address, buffer, size, ref bytesRead) == false) || (bytesRead != size) )
+            {
+                throw new Exception(String.Format("Could not read {0} bytes from address 0x{1:X}", size, address));
+            }
+
+            return buffer;
+        }
+
+        public void WriteMemory(IntPtr address, byte[] buffer)
+        {
+            uint bytesWritten = 0;
+
+
+            if ((WriteProcessMemory(this.processHandle, address, buffer, (uint)buffer.Length, ref bytesWritten) == false) || (bytesWritten != buffer.Length))
+            {
+                throw new Exception(String.Format("Could not write {0} bytes to address 0x{1:X}", buffer.Length, address));
+            }
+
+        }
+
+        private static IntPtr GetProcessHandleByName(string name)
+        {
+            IntPtr handle = IntPtr.Zero;
+            Process[] processes = Process.GetProcessesByName(name);
+            if (processes.Length == 0)
+            {
+                throw new InvalidOperationException(String.Format("Can't find process '{0}'", name));
+            }
+            else if (processes.Length > 1)
+            {
+                throw new InvalidOperationException(String.Format("Too many instances of '{0}'", name));
+            }
+
+            handle = processes[0].Handle;
+
+            Debug.WriteLine(String.Format("Found process handle 0x{0:X} for process {1}", handle, name));
+            return handle;
+        }
+    }
+}
